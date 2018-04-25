75f83bb6308a63e5aae817880e3c3487e7fdac1b
fuzykattan <fuzykattan@gmail.com>
2018-04-25 05:53:23 -0400

    add other.html extends

---
+++ static/components/hello.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="hello">Hello from {{who}}</div>
+</template>
+ 
+<script>
+module.exports = {
+    data: function() {
+        return {
+            who: 'Vue Component!!'
+        }
+    }
+}
+</script>
+ 
+<style>
+.hello {
+    background-color: #eee;
+}
+</style>
